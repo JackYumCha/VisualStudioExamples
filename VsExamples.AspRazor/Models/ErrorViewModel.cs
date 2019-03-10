58cd0d2bb2547d2b1cf2280c8651fd587157e01b
Zhenyu Shi <errisy@gmail.com>
2019-03-10 17:26:12 +1000

    set up angular project

---
+++ VsExamples.AspRazor/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VsExamples.AspRazor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

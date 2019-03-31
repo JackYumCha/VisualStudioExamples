9148cf1321b286e573ab855ae4341bc355f60ed0
Zhenyu Shi <errisy@gmail.com>
2019-03-31 15:22:36 +1000

    postgres username

--- DockerCommands/postgres-new.cmd
+++ DockerCommands/postgres-new.cmd
@@ -1,4 +1,4 @@
 docker stop postgres-server -t 0
 docker rm postgres-server
 docker run --name postgres-server --mount source=postgres-data,target=/var/lib/postgresql/data -p 5499:5432 -e POSTGRES_PASSWORD=root -d postgres
-rem username: root password: root
+rem username: postgres password: root

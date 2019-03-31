877d3a79b0d1da3baa6aa45decea0821b0f12b80
Zhenyu Shi <errisy@gmail.com>
2019-03-31 15:47:57 +1000

    mssql password fix

--- DockerCommands/mssql-new.cmd
+++ DockerCommands/mssql-new.cmd
@@ -1,4 +1,4 @@
 docker stop mssql-server -t 0
 docker rm mssql-server
-docker run --name mssql-server -p 1433:1433 -e sa_password=root -e ACCEPT_EULA=Y -d microsoft/mssql-server-linux:2017-latest
-rem password is root
+docker run --name mssql-server -p 1433:1433 -e SA_PASSWORD=rootR0@t -e ACCEPT_EULA=Y -d microsoft/mssql-server-linux:2017-latest
+rem password is rootR0@t

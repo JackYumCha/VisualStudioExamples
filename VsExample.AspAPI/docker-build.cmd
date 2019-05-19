342552a72475f8a58524e48eb6927421ab607936
Zhenyu Shi <errisy@gmail.com>
2019-05-19 14:50:32 +1000

    aws login and configure

--- VsExample.AspAPI/docker-build.cmd
+++ VsExample.AspAPI/docker-build.cmd
@@ -1,3 +1,5 @@
 dotnet publish VsExample.AspAPI.csproj --framework netcoreapp2.2 -c Release -o ./bin/docker
 docker build ./ -f vsexamples.dockerfile -t vsexamples-image
 docker tag vsexamples-image 714626288919.dkr.ecr.ap-southeast-2.amazonaws.com/vs-examples:%1
+call aws-ecr-login.cmd
+docker push 714626288919.dkr.ecr.ap-southeast-2.amazonaws.com/vs-examples:%1

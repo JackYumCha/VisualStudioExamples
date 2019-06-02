3b8b6b64cd479a7081cb7f32d1e6d33faea61fa4
Zhenyu Shi <errisy@gmail.com>
2019-06-02 17:27:26 +1000

    dynamoDB test

---
+++ VsExamples.Athena.Tests/AWSDynamoDBTests.cs
@@ -0,0 +1,58 @@
+﻿using Autofac;
+using Jack.DataScience.Common;
+using Jack.DataScience.Data.AWSDynamoDB;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Xunit;
+
+namespace VsExamples.Athena.Tests
+{
+    public class AWSDynamoDBTests
+    {
+        [Fact(DisplayName = "Write Data Into DyanmoDB")]
+        public async void WriteDataIntoDynamoDB()
+        {
+            AutoFacContainer autoFacContainer = new AutoFacContainer("dev");
+            autoFacContainer.Setup();
+            var services = autoFacContainer.ContainerBuilder.Build();
+
+            var awsDynamoAPI = services.Resolve<AWSDynamoAPI>();
+
+            await awsDynamoAPI.WriteItem(new Customer()
+            {
+                key = "test02",
+                Name = "John Smith",
+                DateOfBirth = new DateTime(1995, 12, 12),
+                Credit = 600d,
+                CustomerId = 4324235,
+                Phone = "0443636862"
+            });
+        }
+
+        [Fact(DisplayName = "Read Data From DyanmoDB")]
+        public async void ReadDataFromDynamoDB()
+        {
+            AutoFacContainer autoFacContainer = new AutoFacContainer("dev");
+            autoFacContainer.Setup();
+            var services = autoFacContainer.ContainerBuilder.Build();
+
+            var awsDynamoAPI = services.Resolve<AWSDynamoAPI>();
+
+            var customer = await awsDynamoAPI.ReadItem<Customer>("test02");
+
+            Debugger.Break();
+        }
+    }
+
+    public class Customer
+    {
+        public string key { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int CustomerId { get; set; }
+        public double Credit { get; set; }
+    }
+}
--- VsExamples.Athena/Bootstrap.cs
+++ VsExamples.Athena/Bootstrap.cs
@@ -9,6 +9,7 @@ using Jack.DataScience.Storage.AzureBlobStorage;
 using Autofac;
 using Jack.DataScience.Data.AWSAthena;
 using Jack.DataScience.Data.AzureTableStorage;
+using Jack.DataScience.Data.AWSDynamoDB;
 
 namespace VsExamples.Athena
 {
@@ -20,11 +21,13 @@ namespace VsExamples.Athena
             autoFacContainer.RegisterOptions<AWSS3Options>();
             autoFacContainer.RegisterOptions<AzureBlobStorageOptions>();
             autoFacContainer.RegisterOptions<AzureTableStorageOptions>();
+            autoFacContainer.RegisterOptions<AWSDynamoDBOptions>();
             autoFacContainer.RegisterOptions<AWSAthenaOptions>();
             autoFacContainer.RegisterOptions<AthenaOptions>();
             autoFacContainer.ContainerBuilder.RegisterModule<VsExamplesAthenaModule>();
             autoFacContainer.ContainerBuilder.RegisterModule<AzureBlobStorageModule>();
             autoFacContainer.ContainerBuilder.RegisterModule<AzureTableStorageModule>();
+            autoFacContainer.ContainerBuilder.RegisterModule<AWSDynamoDBModule>();
             return autoFacContainer;
         }
     }
